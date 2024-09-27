162af71857ffcf32c17af3bb6555a273e45acf1c
Logan Meyers <theterrarian03@gmail.com>
2024-09-26 23:33:49 -0700

    Added things for Lab 5

---
+++ CPT_S 121/Lab5/Lab5.h
@@ -0,0 +1,19 @@
+/*
+- Logan Meyers
+- 09/26/2024
+- Lab 5 Section #12
+
+- Description: bkjbgkdjrhde
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+char determine_income_bracket(double dollar_amount);
+
+int calc_average(int sum, int count);
+
+int greatest_common_divisor(int num1, int num2);
