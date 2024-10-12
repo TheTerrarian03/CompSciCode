c0123c260d2b49b8b1cb7d2df67c431a4fa8279b
Logan Meyers <theterrarian03@gmail.com>
2024-10-11 23:40:49 -0700

    Worked on PA4

--- CPT_S 121/PAs/PA4/pa4.c
+++ CPT_S 121/PAs/PA4/pa4.c
@@ -24,7 +24,7 @@ The player loses by rolling a 7 before making the point.\
 */
 
 // print methods
-void print_game_rules() {
+void print_game_rules() {  // works
     // shh.. I wantd it to look nice and this was my solution to that NOT
     // including 11 print statements.
     printf("%s%s%s%s%s%s%s%s%s%s%s",
@@ -38,45 +38,123 @@ void print_game_rules() {
            "\nIf the sum is 4, 5, 6, 8, 9, or 10 on the first throw:",
            "\n- sum becomes the player's \"point.\"",
            "\nTo win, you must continue rolling the dice until you \"make your point.\"",
-           "\nThe player loses by rolling a 7 before making the point.");
+           "\nThe player loses by rolling a 7 before making the point.\n\n");
 }
+void print_welcome_screen() {
+    printf("╔═════════════════════════════════════════════════════╗\n");
+    printf("║ ┌                                                 • ║\n");
+    printf("║    Welcome to PA #4 - Craps [by Logan!] Enjoy :D    ║\n");
+    printf("║ •                                                 ┘ ║\n");
+    printf("╚═════════════════════════════════════════════════════╝\n\n");
+}
+// void print_decision_box(char* choices[]) {
+
+// }
 void chatter_messages(int number_rolls,
                       int win_loss_neither,
                       double initial_bank_balance,
                       double current_bank_balance);
+void clear_scr() {
+    #ifndef DISABLE_CLEAR_SCR
+        system(CLEAR_COMMAND);
+    #endif
+}
 
 // input getter methods
-double get_bank_balance() {
-    return get_double_safe_pos("\nPlease enter in your starter bank balance: ");
+double get_bank_balance() {  // works
+    double balance = 0.0;
+    printf("\n> Please enter in your bank balance:\n$ ");
+    scanf(" %lf", &balance);
+    return balance;
 }
-double get_wager_amout();
+double get_wager_amount() {  // works
+    double wager = 0.0;
+    printf("\nPlease enter in your wager balance: ");
+    scanf("%lf", &wager);
+    return wager;
+}
+int get_choice(char* choices[], int num_choices) {
+    printf("Please choose an option by entering a number below and pressing enter:\n");
 
-// input validation loops
-double get_double_safe_pos(char* prompt) {
-    double val;
-    
-    while (1) {
-        val = 0.0;
-
-        if (scanf(" %lf", &val)) {
-            if (val > 0.0) {
-                break;
-            }
-        }
+    for (int i=0; i < num_choices; i++) {
+        printf("%d. %s\n", i+1, choices[i]);
     }
 
-    return val;
+    int choice = 0;
+    printf("$ ");
+    scanf("%d", &choice);
+
+    return choice;
 }
-int read_int_safe(char* prompt);
 
 // calculation methods
-int roll_die();
+int roll_die() {  // works
+    int dice_roll = 0;
+
+    dice_roll = (rand() % 6) + 1;
+
+    return dice_roll;
+}
 double adjust_bank_balance(double bank_balance,
                            double wager_amount,
                            int add_or_subtract);
 
 // comparison methods
-int check_wager_amount(double wager, double balance);
-int calculate_sum_dice(int die1_value, int die2_value);
-int is_win_or_loss_point(int sum_dice);
-int is_point_loss_or_neither(int sum_dice, int point_value);
+int check_wager_amount(double wager, double balance) {  // works
+    return ((wager <= balance) ? 1 : 0);
+}
+int calculate_sum_dice(int die1_value, int die2_value) {  // works
+    return die1_value + die2_value;
+}
+int is_win_loss_or_point(int sum_dice) {  // works
+    // 7 or 11: 1 (win)
+    if (sum_dice == 7 || sum_dice == 11) {
+        return 1;
+    }
+
+    // 2, 3, or 12: 0 (lose)
+    if (sum_dice == 2 || sum_dice == 3 || sum_dice == 12) {
+        return 0;
+    }
+
+    // else: -1 (becomes point)
+    return -1;
+}
+int is_point_loss_or_neither(int sum_dice, int point_value) {  // works
+    // sum == point: 1
+    if (sum_dice == point_value) return 1;
+
+    // sum == 7: 0
+    if (sum_dice == 7) return 0;
+
+    // else: -1
+    return -1;
+}
+
+// gameplay abstractions
+void handle_player_win(double* balance, double wager) {
+    clear_scr();
+
+    *balance += wager;
+
+    printf("Congratulations! You win!!\n\n");
+}
+void handle_player_loss(double* balance, double wager) {
+    clear_scr();
+    
+    *balance -= wager;
+
+    printf("Womp womp, you lose.\n\n");
+}
+void handle_player_make_point(int dice_sum, int* point) {
+    clear_scr();
+    
+    *point = dice_sum;
+
+    printf("Your point is now: %d", *point);
+}
+void handle_player_continue(int sum, int point) {
+    clear_scr();
+
+    printf("So close! (not really).\nYou rolled a:   %d\nbut you need a: %d\nto make your point.\nKeep rolling!\n", sum, point);
+}
--- CPT_S 121/PAs/PA4/pa4.h
+++ CPT_S 121/PAs/PA4/pa4.h
@@ -13,30 +13,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-// print methods
+// constants
+#define DEBUG 1
+#define CLEAR_COMMAND "clear"
+// #define DISABLE_CLEAR_SCR  // un-comment to disable clearing screen, if you want history
+
+#define START_OPTIONS ((const char*[]){"Play Game", "Print Rules", "Exit"})
+#define BALANCE_OPTIONS ((const char*[]){"Keep Current Balance", "Enter New Balance", "Quit to Main Menu"})
+
+/* ---------- print/display methods ---------- */
+
+// Name: print_game_rules
+// Description: Prints game rules
 void print_game_rules();
+
+// Name: print_welcome_screen
+// Description: prints the welcome screen
+void print_welcome_screen();
+// void print_decision_box(char* choices[])
+
+// 
 void chatter_messages(int number_rolls,
                       int win_loss_neither,
                       double initial_bank_balance,
                       double current_bank_balance);
 
-// input getter methods
-double get_bank_balance();
-double get_wager_amout();
-double get_double_safe_pos(char* prompt);
-int read_int_safe(char* prompt);
+// Name: clear_scr
+// Description: clears the terminal screen
+// Adjustment: Adjust CLEAR_COMMAND to change the command to run, for different OS's
+// Adjustment: define DISABLE_CLEAR_SCR to disable clearing the screen
+void clear_scr();
 
-// calculation methods
+/* --------- input getter methods ----------*/
+
+// Name: get_bank_balance
+// Description: prompts and asks for player's bank balance
+double get_bank_balance();
+
+// Name: get_wager_amount
+// Description: prompts and asks for the player's wager
+double get_wager_amount();
+
+// Name: get_choice
+// Description: Prompts and asks for the player's choice
+// Parameter: char* choices[], an array of strings for choices to give the player
+// Parameter: int num_choices, the number of prompts to print from the choices array
+// Returns: integer choice with range [1, num_choices+]
+int get_choice(char* choices[], int num_choices);
+
+/* --------- calculation methods ---------*/
+
+// Name: roll_die
+// Description: rolls a die
+// Returns: int result, with range [1, 6]
 int roll_die();
+
+// Name: adjust_bank_balance
+// Description: Adjusts the bank balance based on wager and whether to add or sub
+// Notes: unused
 double adjust_bank_balance(double bank_balance,
                            double wager_amount,
                            int add_or_subtract);
 
-// comparison methods
+/* --------- comparison methods ---------*/
+
 int check_wager_amount(double wager, double balance);
 int calculate_sum_dice(int die1_value, int die2_value);
-int is_win_or_loss_point(int sum_dice);
+int is_win_loss_or_point(int sum_dice);
 int is_point_loss_or_neither(int sum_dice, int point_value);
+
+// gameplay abstractions
+void handle_player_win(double* balance, double wager);
+void handle_player_loss(double* balance, double wager);
+void handle_player_make_point(int dice_sum, int* point);
+void handle_player_continue(int sum, int point);
 
 #endif
