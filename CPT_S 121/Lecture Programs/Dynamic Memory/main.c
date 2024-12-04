53077772b1ff14b9a2f4028e6a1dc50dec5c2343
Logan Meyers <theterrarian03@gmail.com>
2024-12-04 11:10:51 -0800

    Worked on Lecture Program

--- CPT_S 121/Lecture Programs/Dynamic Memory/main.c
+++ CPT_S 121/Lecture Programs/Dynamic Memory/main.c
@@ -10,25 +10,38 @@
 // where any Node has data and a pointer to another Node.
 // There is a head/start pointer to the first Node.
 
+// TYPEDEFS
+
 typedef struct node {
     char grocery_item[50];
     struct node *link_ptr;  // self-referential struct (not recursion)
 } Node;
 
+// FUNCTION HEADERS
+
 Node *create_node(char *item);
 int insert_at_front(Node **list_ptr, char *item);  // list operation
+char *delete_at_front(Node **list_ptr);
+void print_list(Node *list_ptr);
+
+// MAIN
 
 int main() {
     Node *head_ptr = NULL;
     int success = 0;
 
-    success = insert_at_front(head_ptr, "bananas");
+    success = insert_at_front(&head_ptr, "bread");
+    success = insert_at_front(&head_ptr, "eggs");
+    success = insert_at_front(&head_ptr, "bananas");
+
+    print_list(head_ptr);
 
     // printf("Success: %d, Item: %s\n", success, head_ptr->grocery_item);
-    printf("This is horrible :D");
 
     return 0;
 }
+
+// FUNCTIONS
 
 Node *create_node(char *item) {
     Node *mem_ptr = malloc(sizeof(Node));
@@ -55,7 +68,41 @@ int insert_at_front(Node **list_ptr, char *item) {
             mem_ptr->link_ptr = list_ptr;
             *list_ptr = &mem_ptr;
         }
+        strcpy(mem_ptr->grocery_item, item);
     }
 
     return success;
+}
+
+char *delete_at_front(Node **list_ptr) {
+    // make pointer refering to first node in list
+    Node *tmp_ptr = *list_ptr;
+
+    // Update head pointer through list pointer
+    // Set head pointer to next node in sequence
+    *list_ptr = tmp_ptr -> link_ptr;
+
+    // copy string to return
+    char data[50] = "";
+    strcpy(data, (*list_ptr)->grocery_item);
+
+    // delete node
+    free(tmp_ptr);
+
+    // return string removed
+    return data;
+}
+
+void print_list(Node *list_ptr) {
+    printf("-->");
+
+
+    int c = 0;
+    
+    while (list_ptr != NULL) {
+        printf("%s --> ", list_ptr->grocery_item);
+        list_ptr = list_ptr->link_ptr;
+
+    }
+    putchar('\n');
 }
