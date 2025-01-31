9c60ab8fe92a2b128ea9b19fb922b62357dfe387
Logan Meyers <theterrarian03@gmail.com>
2025-01-30 19:17:52 -0800

    Exam 1 review stuff'

--- 122/QuizCode/Exam1Review/main.c
+++ 122/QuizCode/Exam1Review/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node {
     int data;
@@ -6,8 +7,73 @@ typedef struct node {
     struct node* pPrev;
 } Node;
 
+void printList(Node *pList);
+void printBack(Node *pTail);
+Node *getTail(Node *pList);
+Node *makeNode(int data);
+Node *insertAtFront(Node **pList, int data);
+Node *mergeList(Node **pList1, Node **pList2);
+
+int main() {
+    Node *pList1 = makeNode(6);
+
+    pList1 = insertAtFront(&pList1, 5);
+    pList1 = insertAtFront(&pList1, 4);
+    pList1 = insertAtFront(&pList1, 3);
+    pList1 = insertAtFront(&pList1, 2);
+    pList1 = insertAtFront(&pList1, 1);
+
+    Node *pList2 = makeNode(10);
+
+    pList2 = insertAtFront(&pList2, 9);
+    pList2 = insertAtFront(&pList2, 8);
+    pList2 = insertAtFront(&pList2, 7);
+
+    printList(pList1);
+    printList(pList2);
+    
+    mergeList(&pList1, &pList2);
+    printList(pList1);
+    printBack(getTail(pList1));
+
+}
+
+void printList(Node *pList) {
+    if (pList == NULL) {
+        printf("List empty\n");
+        return;
+    }
+
+    printf("%d\n", pList->data);
+    printList(pList->pNext);
+}
+void printBack(Node *pTail) {
+    if (pTail == NULL) {
+        printf("List empty\n");
+        return;
+    }
+
+    printf("%d\n", pTail->data);
+    printBack(pTail->pPrev);
+}
+
+Node *getTail(Node *pList) {
+    if (pList == NULL) {
+        printf("empty list\n");
+        return NULL;
+    }
+
+    Node *pCur = pList;
+
+    while (pCur->pNext != NULL) {
+        pCur = pCur->pNext;
+    }
+
+    return pCur;
+}
+
 Node *makeNode(int data) {
-    Node *pNew = malloc(sizeof(Node));
+    Node *pNew = (Node *)malloc(sizeof(Node));
 
     if (!pNew) return NULL;
 
@@ -18,62 +84,48 @@ Node *makeNode(int data) {
     return pNew;
 }
 
-void insert_front(Node **pHead, int data) {
-    // empty list
-    if (*pHead == NULL) {
-        *pHead = makeNode(data);
-        return;
-    }
+Node *insertAtFront(Node **pList, int data) {
+    Node *pMem = makeNode(data);
 
-    // not empty
-    Node *pNew = makeNode(data);
-    pNew->pNext = pHead;
-    (*pHead)->pPrev = pNew;
-    *pHead = pNew;
+    pMem->pNext = *pList;
+
+    if (pMem->pNext) (*pList)->pPrev = pMem;
+
+    // *pList = pMem;
+
+    return pMem;
 }
 
-void insert_back(Node **pHead, int data) {
-    // empty
-    if (*pHead == NULL) {
-        *pHead = makeNode(data);
-        return;
+Node *mergeList(Node **pList1, Node **pList2) {
+    if (*pList1 == NULL) {
+        return *pList2;
     }
 
-    Node *pCur = *pHead;
+    Node *pCur = *pList1;
+    // Node *pPrev = NULL;
 
     while (pCur->pNext != NULL) {
+        // pPrev = pCur;
         pCur = pCur->pNext;
     }
 
-    Node *pNew = makeNode(data);
-    pCur->pNext = pNew;
-    pCur->pPrev = pCur;
+    // pPrev->pNext = *pList2;
+    pCur->pNext = *pList2;
+    (*pList2)->pPrev = pCur;
+    return *pList1;
 }
-
-/*
-insert order:
-
-empty, make node and set head
-
-data less than current head, insert front
-
-find spot by incrementing while new data < current data
-but check for end of list, insert at back
-
-somewhere in the middle,
-make node
-
-new->next to current->next
-new->prev to current->prev
-
-current->next to new
-if (new->next) exists, new->next->prev = new
-*/
-
-int isEmpty(Node *pList) {
-
-}
-
-int main() {
-
-}
+// Node* mergeList(Node** pList1, Node** pList2){
+//     Node *pCur = *pList1;
+//     Node *pPrev = NULL;
+//     while(pCur != NULL){
+//         pPrev = pCur;
+//         pCur = pCur->pNext;
+//     }
+//     if(pPrev != NULL){
+//         pPrev->pNext = *pList2;
+//         return *pList1;
+//     }
+//     else{
+//         return *pList2;
+//     }
+// }   
