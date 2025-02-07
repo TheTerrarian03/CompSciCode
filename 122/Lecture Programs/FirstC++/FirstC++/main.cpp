46165ab4cc00f4a50339dfdace82666b9768e9b5
Logan Meyers <theterrarian03@gmail.com>
2025-02-07 11:10:17 -0800

    Worked on First C++ and Rectangle program in class

--- 122/Lecture Programs/FirstC++/FirstC++/main.cpp
+++ 122/Lecture Programs/FirstC++/FirstC++/main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <string>
+
+// function overloading
+int add(int n1, int n2);
+std::string add(std::string s1, std::string s2);
+
 
 int main() {
+	std::cout << "Result of int add: " << add(5, 6) << std::endl;
+	std::cout << "Result of std::string add: " << add("Hello ", "World!") << std::endl;
+
+
+	int num = 50;
+	int &refNum = num;  // reference operator
+	// we have created an alias for an existing variable/object
+	num = 75;
+
+	std::cout << "Ref num contents: " << refNum << std::endl;
+	std::cout << "Address of num: " << &num << " Address of refNum: " << &refNum << std::endl;
+
 	std::cout << "Hello world!" << std::endl;
+
+	std::string str = "";
+	std::cout << "Enter a C++ standard string: ";
+	std::cin >> str;
 	
+	std::cout << "You entered: \"" << str << "\"." << std::endl;
+
 	return 0;
+}
+
+int add(int n1, int n2) {
+	return n1 + n2;
+}
+std::string add(std::string s1, std::string s2) {
+	return s1 + s2;
 }
---
+++ 122/Lecture Programs/Rectangle/Rectangle/Rectangle.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>  // input output operations; cout cin
+// using namespace std;  // don't do
+using std::cin;  // cin >> str instead of std::cin
+using std::cout;
+using std::endl;
+
+class Rectangle {
+
+};
