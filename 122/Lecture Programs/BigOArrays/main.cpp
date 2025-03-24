7dd727c4ac0deb03b3a2a71163873d66e11a9a42
Logan Meyers <theterrarian03@gmail.com>
2025-03-24 12:09:24 -0700

    added Big O Arrays lecture code

---
+++ 122/Lecture Programs/BigOArrays/main.cpp
@@ -0,0 +1,19 @@
+// This example studies sequential/linear search, binary search,
+// selection sort, and bubble sort to explore Big-O analysis
+
+// History: 
+
+#include "BigO.h"
+
+int main(void)
+{
+	BigO::SortsAndSearches<int> myObj;
+	bool found = false;
+	
+	int list[5] = { 5, 3, 0, -1, 8 }, pos = 0;
+
+	//cout << "result of sequential search: " << found << ", " << pos << endl;
+
+
+	return 0;
+}
